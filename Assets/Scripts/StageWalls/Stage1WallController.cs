7e1c9540072dea9a23118ee9ca660d7f64f3779a
Yusuke Morioka <morioka52513334@gmail.com>
2021-05-28 08:35:42 +0900

    commit
    Stage1Wallを改良

--- Assets/Scripts/StageWalls/Stage1WallController.cs
+++ Assets/Scripts/StageWalls/Stage1WallController.cs
@@ -7,12 +7,17 @@ public class Stage1WallController : MonoBehaviour
     public GameObject enemy1;
     public GameObject stageWall;
 
+    public GameObject nextEnemy1, nextEnemy2;
+
     // Update is called once per frame
     void Update()
     {
         if(enemy1 == null)
         {
             Destroy(stageWall);
+            nextEnemy1.SetActive(true); 
+            nextEnemy2.SetActive(true);
+
         }
 
 
